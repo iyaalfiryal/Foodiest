2fcfaba150f81e85f42a6f5077d31af2e16b8e99
veroanggra <veronicaputri497@gmail.com>
2020-02-17 16:02:02 +0700

    add current location

--- app/src/main/java/com/veronica/idn/foodiest/fragment/LocationFragment.kt
+++ app/src/main/java/com/veronica/idn/foodiest/fragment/LocationFragment.kt
@@ -2,6 +2,7 @@ package com.veronica.idn.foodiest.fragment
 
 
 import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
@@ -16,14 +17,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
 import com.veronica.idn.foodiest.R
 import com.veronica.idn.foodiest.model.Foods
 import com.veronica.idn.foodiest.model.Locations
 import kotlinx.android.synthetic.main.fragment_location.*
+import java.util.jar.Manifest
 
 /**
  * A simple [Fragment] subclass.
@@ -31,10 +36,15 @@ import kotlinx.android.synthetic.main.fragment_location.*
 class LocationFragment : Fragment(), OnMapReadyCallback {
     private val locations = ArrayList<Locations>()
 
+    private val TAG = this.javaClass.simpleName
+
     private lateinit var mapView: GoogleMap
 
     companion object {
+
         var mapFragment: SupportMapFragment? = null
+        const val RQ_LOCATION_PERMISSION = 1
+
     }
 
     override fun onCreateView(
@@ -51,40 +61,83 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         return view
     }
 
-    fun getListLocation(): ArrayList<Locations> {
-        val dataTitle = resources.getStringArray(R.array.title)
-        val dataLat = resources.getStringArray(R.array.lat)
-        val dataLng = resources.getStringArray(R.array.lng)
-
-
-        val listLocation = ArrayList<Locations>()
-        for (position in dataTitle.indices) {
-            val loc = Locations(
-                dataTitle[position],
-                dataLat[position].toDouble(),
-                dataLng[position].toDouble()
-
-            )
-            listLocation.add(loc)
-        }
-        return listLocation
-    }
-
 
     override fun onMapReady(p0: GoogleMap?) {
         mapView = p0!!
         val idn = LatLng(-6.174760, 106.827070)
-
-
-
         mapView.addMarker(
-            MarkerOptions().position(idn).title("IDN").icon(
-                BitmapDescriptorFactory.fromResource(R.drawable.store)
-            )
-        ).showInfoWindow()
+            MarkerOptions().position(idn)
+                .title("IDN").icon(
+                    BitmapDescriptorFactory
+                        .fromResource(R.drawable.store)
+                )
+        )
+            .showInfoWindow()
         mapView.moveCamera(CameraUpdateFactory.newLatLngZoom(idn, 16.0f))
-        mapView.animateCamera(CameraUpdateFactory.newLatLngZoom(idn, 16.0f))
+        mapView.isTrafficEnabled = true
+        setMapClick(mapView)
+        setMapLongClick(mapView)
+        enabledMyLocation()
+    }
 
+    private fun enabledMyLocation() {
+        if (isPermissionGranted()) {
+            mapView.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                RQ_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    private fun setMapLongClick(mapView: GoogleMap) {
+        mapView.setOnMapLongClickListener { it ->
+            val snippetValue = "${it.latitude.toFloat()} ${it.longitude.toFloat()}"
+            mapView.addMarker(
+                MarkerOptions().position(it).title("Pinned").icon(
+                    BitmapDescriptorFactory.fromResource(
+                        R.drawable.store
+                    )
+                ).snippet(snippetValue)
+            ).showInfoWindow()
+        }
+
+    }
+
+    private fun setMapClick(mapView: GoogleMap) {
+        mapView.setOnPoiClickListener { it ->
+            val poiMarker = mapView.addMarker(
+                MarkerOptions().position(it.latLng)
+                    .title(it.name)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.store))
+                    .snippet(it.placeId)
+            )
+            poiMarker.showInfoWindow()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RQ_LOCATION_PERMISSION){
+            if (grantResults.isNotEmpty()&& (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                enabledMyLocation()
+            }
+        }
     }
 }
 
